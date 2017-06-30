ec9e27bc64a872d0abcd92e3722925b0d72eea84
Aleksandrs Muravjovs <kavalskij@gmail.com>
2017-06-30 16:54:05 +0300

    Added new tableViewControlled "GroupLibararyController" for grouping main E substances in to 9 groupes.

---
+++ GroupLibraryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupLibraryViewController.h
+//  E-vielas
+//
+//  Created by Aleksandrs Muravjovs on 30/06/2017.
+//  Copyright © 2017 Aleksandrs Muravjovs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupLibraryViewController : UITableViewController
+
+@end
